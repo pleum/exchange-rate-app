d902b08a31366f3365599bd4fd0cbf67111a4585
Pleum <pleumofficial@gmail.com>
2019-02-17 22:30:39 +0700

    upload code

--- src/App.js
+++ src/App.js
@@ -1,28 +1,108 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+const exchangeRatesAPI = 'https://api.exchangeratesapi.io/latest?base=';
+
+function App() {
+  const [currencyBase] = useState('THB');
+  const [rateBase, setRateBase] = useState(1);
+  const [data, setData] = useState({});
+  const [isFinish, setFinish] = useState(false);
+
+  const fetchCurrency = async () => {
+    try {
+      const response = await axios.get(exchangeRatesAPI + currencyBase);
+      if (response.status === 200) {
+        setData(response.data);
+        setFinish(true);
+      }
+    } catch (e) {}
+  };
+
+  useEffect(() => {
+    fetchCurrency();
+  }, [currencyBase]);
+
+  const handleRateChange = e => {
+    setRateBase(e.target.value);
+  };
+
+  return (
+    <div>
+      <header class="tc">
+        <h1 class="mb0">อัตราแลกเปลี่ยน</h1>
+        <p class="inline">
+          สร้างด้วย{' '}
           <a
-            className="App-link"
+            class="dark-blue link dim"
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            React.js
           </a>
-        </header>
-      </div>
-    );
-  }
+          ,{' '}
+          <a
+            class="dark-blue link dim"
+            href="https://tachyons.io"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            tachyons.css
+          </a>
+          {' และ '}
+          <a
+            class="dark-blue link dim"
+            href="https://exchangeratesapi.io"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            exchangeratesapi
+          </a>
+        </p>
+      </header>
+      <section class="ph3 mb4">
+        <form class="mw6 center mb2">
+          <input type="text" onChange={handleRateChange} value={rateBase} /> THB
+        </form>
+        <article class="mw6 center">
+          <div class="flex flex-column">
+            {isFinish ? (
+              <div>
+                <div class="mb2">
+                  {Object.keys(data.rates)
+                    .filter(currcency => currcency !== currencyBase)
+                    .map(currcency => (
+                      <article
+                        class="dt pa2 bb b--black-05 w-100"
+                        key={currcency}
+                      >
+                        <div class="dtc v-mid">
+                          <h1 class="f6 f5-ns lh-title black mv0">
+                            {currcency}
+                          </h1>
+                          <h2 class="f6 fw4 mt0 mb0 black-60">
+                            {data.rates[currcency]}
+                          </h2>
+                        </div>
+                        <div class="dtc v-mid">
+                          <span class="button-reset bg-green pa2 white fr pv1">
+                            {data.rates[currcency] * rateBase}
+                          </span>
+                        </div>
+                      </article>
+                    ))}
+                </div>
+                <div class="tc">Lastest update {data.date}</div>
+              </div>
+            ) : (
+              <div class="tc">Loading...</div>
+            )}
+          </div>
+        </article>
+      </section>
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'tachyons';
 import './index.css';
+
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
